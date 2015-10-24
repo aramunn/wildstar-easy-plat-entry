e6a33a3cf6d2d45a7327671a77a4a41a8cdde0d2
Aramunn <aramunn@users.noreply.github.com>
2015-10-24 18:20:29 -0500

    edit box shows up on click

--- EasyPlatEntry.lua
+++ EasyPlatEntry.lua
@@ -2,6 +2,17 @@ require "Window"
 
 local EasyPlatEntry = {}
 
+local Hooks = {
+  {
+    addonToHook = "MarketplaceAuction",
+    methodToHook = "OnToggleAuctionWindow",
+    pathToWindowToHook = {
+      "SellContainer",
+      "CreateBuyoutInputBox",
+    },
+  },
+}
+
 function EasyPlatEntry:new(o)
   o = o or {}
   setmetatable(o, self)
@@ -21,6 +32,24 @@ end
 function EasyPlatEntry:OnDocumentReady()
   if self.xmlDoc == nil then return end
   if not self.xmlDoc:IsLoaded() then return end
+  for idx, hook in ipairs(Hooks) do
+    local addon = Apollo.GetAddon(hook.addonToHook)
+    if addon ~= nil then
+      local method = addon[hook.methodToHook]
+      addon[hook.methodToHook] = function (...)
+        method(...)
+        local cashWindow = addon.wndMain
+        for idx, child in ipairs(hook.pathToWindowToHook) do
+          cashWindow = cashWindow:FindChild(child)
+        end
+        cashWindow:AddEventHandler("MouseButtonDown", "EasyPlatEntryHook")
+      end
+      addon["EasyPlatEntryHook"] = function (wndHandler, wndControl)
+        self.wndMain = Apollo.LoadForm(self.xmlDoc, "TextToMoneyForm", wndControl, self)
+        self.wndMain:FindChild("EditBox"):SetFocus()
+      end
+    end
+  end
 end
 
 local EasyPlatEntryInst = EasyPlatEntry:new()
